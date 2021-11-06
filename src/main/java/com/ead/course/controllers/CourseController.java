fb1f56116c2d81591400fb04d4c971cd16b09d9a
tnicacio <nicacio.t92@gmail.com>
2021-11-06 13:43:38 -0300

    Beginning of API Composition Pattern module

--- src/main/java/com/ead/course/controllers/CourseController.java
+++ src/main/java/com/ead/course/controllers/CourseController.java
@@ -73,7 +73,11 @@ public class CourseController {
 
     @GetMapping
     public ResponseEntity<Page<CourseModel>> getAllCourses(SpecificationTemplate.CourseSpec spec,
-                                                           @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
+                                                           @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable,
+                                                           @RequestParam(required = false) UUID userId) {
+        if (userId != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(courseService.findAll(SpecificationTemplate.courseUserId(userId).and(spec), pageable));
+        }
         return ResponseEntity.status(HttpStatus.OK).body(courseService.findAll(spec, pageable));
     }
 
---
+++ src/main/java/com/ead/course/models/CourseUserModel.java
@@ -0,0 +1,32 @@
+package com.ead.course.models;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = "tb_courses_users",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_course_id_user_id", columnNames = { "course_id", "user_id" })
+        }
+)
+public class CourseUserModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "course_id")
+    private CourseModel course;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+}
